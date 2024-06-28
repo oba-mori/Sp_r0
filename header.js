f780846103659065d894c445c0c092975419a3de
oba-mori <99237421+oba-mori@users.noreply.github.com>
2024-06-28 16:51:57 +0900

    Add files via upload

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#headerContent').load('header.html header', function(response, status, xhr) {
+  $('header').load('header.html header', function(response, status, xhr) {
       if (status == "error") {
           console.log("Error: " + xhr.status + ": " + xhr.statusText);
       }
